a20df61b44ec8f7aee27d707a7d5860347cef804
Usama Razzaq <usamarazzaq9014@gmail.com>
2024-08-13 15:26:52 +0500

    Added popular products section in Home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TopHeader from "./components/Header/TopHeader";
 import BottomHeader from "./components/Header/BottomHeader";
 import Row from "./components/common/Row";
 import HeroSection from "./components/Home/HeroSection";
+import Container from "./components/common/Container";
+import PopularProducts from "./components/Home/PopularProducts";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <TopHeader />
       <BottomHeader />
       <HeroSection />
+      <PopularProducts />
     </div>
   );
 }
